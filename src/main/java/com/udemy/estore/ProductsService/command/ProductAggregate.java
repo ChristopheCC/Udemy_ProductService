095c231397adbd9c49fa14cc8098709563caa207
ChristopheCC <christophe.chane@gmail.com>
2023-01-01 15:37:20 +0800

    Section 16 : Orchestration based saga

--- src/main/java/com/udemy/estore/ProductsService/command/ProductAggregate.java
+++ src/main/java/com/udemy/estore/ProductsService/command/ProductAggregate.java
@@ -1,6 +1,8 @@
 package com.udemy.estore.ProductsService.command;
 
 import com.udemy.estore.ProductsService.core.events.ProductCreatedEvent;
+import com.udemy.estore.core.commands.ReserveProductCommand;
+import com.udemy.estore.core.events.ProductReservedEvent;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.modelling.command.AggregateIdentifier;
@@ -41,11 +43,33 @@ public class ProductAggregate {
 
     }
 
+    @CommandHandler
+    public void handle(ReserveProductCommand reserveProductCommand){
+        if (quantity < reserveProductCommand.getQuantity()){
+            throw new IllegalArgumentException("Insufficient number of item in stocks");
+        }
+
+        ProductReservedEvent productReservedEvent = ProductReservedEvent.builder()
+                .orderId(reserveProductCommand.getOrderId())
+                .productId(reserveProductCommand.getProductId())
+                .quantity(reserveProductCommand.getQuantity())
+                .userId(reserveProductCommand.getUserId())
+                .build();
+
+        AggregateLifecycle.apply(productReservedEvent);
+
+    }
+
     @EventSourcingHandler
     public void on(ProductCreatedEvent productCreatedEvent){
         this.productId = productCreatedEvent.getProductId();
         this.price = productCreatedEvent.getPrice();
         this.title = productCreatedEvent.getTitle();
         this.quantity = productCreatedEvent.getQuantity();
+    }
+
+    @EventSourcingHandler
+    public void on(ProductReservedEvent productReservedEvent){
+        this.quantity -= productReservedEvent.getQuantity();
     }
 }
--- src/main/java/com/udemy/estore/ProductsService/query/ProductEventsHandler.java
+++ src/main/java/com/udemy/estore/ProductsService/query/ProductEventsHandler.java
@@ -3,15 +3,20 @@ package com.udemy.estore.ProductsService.query;
 import com.udemy.estore.ProductsService.core.data.ProductEntity;
 import com.udemy.estore.ProductsService.core.data.ProductsRepository;
 import com.udemy.estore.ProductsService.core.events.ProductCreatedEvent;
+import com.udemy.estore.core.events.ProductReservedEvent;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
 import org.axonframework.messaging.interceptors.ExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 @ProcessingGroup("product-group")
 public class ProductEventsHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductEventsHandler.class);
 
     private final ProductsRepository productsRepository;
 
@@ -38,10 +43,21 @@ public class ProductEventsHandler {
         } catch (IllegalArgumentException ex){
             ex.printStackTrace();
         }
-        if (true){
-            throw new Exception("An error took place in the Event Handler class");
-        }
+//        if (true)
+//            throw new Exception("An error took place in the Event Handler class");
+//        }
 
+    }
+
+    @EventHandler
+    public void on(ProductReservedEvent productReservedEvent){
+        ProductEntity productEntity = productsRepository.findByProductId(productReservedEvent.getProductId());
+        productEntity.setQuantity(productEntity.getQuantity() - productReservedEvent.getQuantity());
+        productsRepository.save(productEntity);
+
+
+        LOGGER.info("ProductReservedEvent is called for orderId : " + productReservedEvent.getOrderId()
+                + " and productId : "+ productReservedEvent.getProductId());
     }
 
 }
